e812ce60c07dae03d7c2a462641460d3b4143c61
Tyler Liu <tyler4long@gmail.com>
2024-05-04 11:58:10 -0700

    skip

--- src/app.tsx
+++ src/app.tsx
@@ -6,49 +6,56 @@ import type Game from './game';
 import Card from './card';
 
 const { Title } = Typography;
+const cardBackImage = Card.backImage();
 
 const App = (props: { game: Game }) => {
   const { game } = props;
-  const youPlayer = game.findPlayer('You');
-  const aiPlayer = game.findPlayer('AI');
-  const isYourTurn = game.currentTurnPlayer === youPlayer;
-  const cardBackImage = Card.backImage();
-  const render = () => (
-    <>
-      <Title>憋驴</Title>
-      <Space direction="vertical">
-        <Button onClick={() => game.moveOn()}>Move on</Button>
-        <Title level={2}>AI</Title>
-        <div>
-          {aiPlayer.hand.map((card) => (
-            <img key={`${card.suit}-${card.rank}`} src={cardBackImage} width="128px" />
-          ))}
-        </div>
-        <Divider />
-        <Alert message={isYourTurn ? "It's your turn." : "It's AI's turn."} type={isYourTurn ? 'success' : 'error'} />
-        <Divider />
-        <Title level={2}>你</Title>
-        <div>
-          {youPlayer.hand.map((card) => (
-            <img key={`${card.suit}-${card.rank}`} src={card.image} width="128px" />
-          ))}
-        </div>
-        {isYourTurn && game.deck.cards.length > 0 && (
-          <>
-            <Button
-              block
-              size="large"
-              onClick={() => {
-                youPlayer.hand.push(game.deck.pop());
-              }}
-            >
-              摸牌
+  const render = () => {
+    const youPlayer = game.findPlayer('You');
+    const aiPlayer = game.findPlayer('AI');
+    const isYourTurn = game.currentTurnPlayer === youPlayer;
+    return (
+      <>
+        <Title>憋驴</Title>
+        <Space direction="vertical">
+          <Title level={2}>AI</Title>
+          <div>
+            {aiPlayer.hand.map((card) => (
+              <img className="card-img" key={`${card.suit}-${card.rank}`} src={cardBackImage} width="128px" />
+            ))}
+          </div>
+          <Divider />
+          <Alert message={isYourTurn ? "It's your turn." : "It's AI's turn."} type={isYourTurn ? 'success' : 'error'} />
+          <Divider />
+          <Title level={2}>你</Title>
+          {isYourTurn && (
+            <>
+              <Button
+                block
+                size="large"
+                onClick={() => {
+                  youPlayer.hand.push(game.deck.pop());
+                }}
+                disabled={game.deck.cards.length === 0}
+              >
+                摸牌
+              </Button>
+            </>
+          )}
+          {isYourTurn && game.deck.cards.length === 0 && (
+            <Button block size="large" onClick={() => game.moveOn()}>
+              跳过
             </Button>
-          </>
-        )}
-      </Space>
-    </>
-  );
+          )}
+          <div>
+            {youPlayer.hand.map((card) => (
+              <img className="card-img" key={`${card.suit}-${card.rank}`} src={card.image} width="128px" />
+            ))}
+          </div>
+        </Space>
+      </>
+    );
+  };
   return auto(render, props);
 };
 
